c6b3acc3b70d66dbe2f2c9393cd99700dc4db923
NorthBlue333 <north333@free.fr>
2020-10-12 11:42:23 +0200

    chore: init project

---
+++ src/router/routes.ts
@@ -0,0 +1,25 @@
+type RouteName = 'list' | 'profile' | 'details'
+
+interface Route {
+  name: RouteName
+  path: string
+  title: string
+}
+
+export const list: Route = {
+  name: 'list',
+  path: '/liste',
+  title: 'Liste',
+}
+
+export const profile: Route = {
+  name: 'profile',
+  path: '/profile',
+  title: 'Profile',
+}
+
+export const details: Route = {
+  name: 'details',
+  path: '/details',
+  title: 'Détails',
+}
