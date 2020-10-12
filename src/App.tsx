e0e7d1f534a39a40be892c3f46d9d7d9169d21ca
NorthBlue333 <north333@free.fr>
2020-10-12 12:05:21 +0200

    chore: update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,47 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom'
+import { IonApp, IonRouterOutlet } from '@ionic/react'
+import { IonReactRouter } from '@ionic/react-router'
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import '@ionic/react/css/core.css'
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import '@ionic/react/css/normalize.css'
+import '@ionic/react/css/structure.css'
+import '@ionic/react/css/typography.css'
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import '@ionic/react/css/padding.css'
+import '@ionic/react/css/float-elements.css'
+import '@ionic/react/css/text-alignment.css'
+import '@ionic/react/css/text-transformation.css'
+import '@ionic/react/css/flex-utils.css'
+import '@ionic/react/css/display.css'
 
 /* Theme variables */
-import './theme/variables.css';
-import { routes } from './router/routes';
+import './theme/variables.css'
+import { RouteName, routes } from './router/routes'
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route name={routes.list} component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
+        {Object.keys(routes).map((routeName) => (
+          <Route
+            path={routes[routeName as RouteName].path}
+            component={routes[routeName as RouteName].component}
+            exact={true}
+          />
+        ))}
+        <Route
+          exact
+          path="/"
+          render={() => <Redirect to={routes.list.path} />}
+        />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
-);
+)
 
-export default App;
+export default App
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,27 +1,33 @@
-type RouteName = 'list' | 'profile' | 'details'
+import Home from '../pages/Home'
+
+export type RouteName = 'list' | 'profile' | 'details'
 
 interface Route {
   name: RouteName
   path: string
   title: string
+  component: React.FC
 }
 
 export const list: Route = {
   name: 'list',
   path: '/liste',
   title: 'Liste',
+  component: Home,
 }
 
 export const profile: Route = {
   name: 'profile',
   path: '/profile',
   title: 'Profile',
+  component: Home,
 }
 
 export const details: Route = {
   name: 'details',
   path: '/details',
   title: 'Détails',
+  component: Home,
 }
 
 export const routes: {
